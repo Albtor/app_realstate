3afece13ad6e4aac6dfcee8ca6c7de70bcde7015
albtordev <albtordev@gmail.com>
2026-06-30 20:13:23 +0100

    Link to property page

---
+++ app/src/main/java/com/example/app_realstate/PropertyPageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.app_realstate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PropertyPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.property_page)
+    }
+}
